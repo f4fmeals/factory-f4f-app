986c27e6f4f0945edc88abf424f79f7b028b3c4e
Frederico Branco <fredericobranco@MacBook-Pro-de-Frederico-2.local>
2026-05-07 19:12:01 +0100

    novo url cloudflared

--- app/cozinha/page.tsx
+++ app/cozinha/page.tsx
@@ -587,7 +587,7 @@ export default function Cozinha() {
     if (data) setRegistosDesinfeccao(prev => ({ ...prev, [tarefaPreparacaoId]: { ...novoEstado, id: data.id } }))
   }
 
-  const PRINTER_URL = 'https://expansys-football-complement-numerical.trycloudflare.com/print'
+  const PRINTER_URL = 'https://legacy-arthritis-copyrighted-remarks.trycloudflare.com/print'
 
   function gerarZPL(dados: {
     componenteDestino: string
